78173dc8ff9672f03b2cb6782a38914e66113291
PorkChopExpress86 <blake.b.8726@gmail.com>
2022-05-03 02:50:05 -0500

    Template to start javafx project

---
+++ src/view/controller/FirstScene.java
@@ -0,0 +1,24 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FirstScene implements Initializable {
+
+    public Label theLabel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("I am initialized.");
+        theLabel.setText("Initialized");
+    }
+
+    public void onButtonAction(ActionEvent actionEvent) {
+        System.out.println("I am clicked!");
+        theLabel.setText("Hello World GUI!");
+    }
+}
