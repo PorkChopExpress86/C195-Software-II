31e008b47da55e3d9acf1f596d75328aefdbd02f
Porkchopexpress86 <blakeb.8726@gmail.com>
2022-05-28 15:32:30 -0500

    DBUser class uses a perpare statment to check the user credential at login, return true if correct and false if incorrect.

---
+++ src/Database/DBUser.java
@@ -0,0 +1,36 @@
+package Database;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBUser {
+    static String currentUser;
+
+    public static boolean userLogin(String username, String password) {
+        try {
+            String sql = "SELECT * FROM users WHERE User_Name = ? AND PASSWORD = ?";
+            PreparedStatement ps = JDBC.getConnection().prepareStatement(sql);
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                setCurrentUser(username);
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static void setCurrentUser(String username) {
+        currentUser = username;
+    }
+
+    public static String getCurrentUser() {
+        return currentUser;
+    }
+}
