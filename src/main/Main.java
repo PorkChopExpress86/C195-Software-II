c59db532dad262201650c0f179c45d634587464c
Porkchopexpress86 <blakeb.8726@gmail.com>
2022-05-28 15:37:35 -0500

    Changed the primaryStage size for startup

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,5 @@
 package main;
 
-import Database.FruitsQuery;
 import Database.JDBC;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -8,33 +7,25 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Objects;
 
 public class Main extends Application {
 
     public static void main(String[] args) throws SQLException {
-//        DBConnection.startConnection();
-//        launch(args);
-//        DBConnection.closeConnection();
-
-        // Test connection to the database
-        JDBC.openConnection();
-
-        // Try to insert a fruit
-        int rowsAffected = FruitsQuery.insert("Cherries", 1);
-        if (rowsAffected > 0) {
-            System.out.println("Insert Successful!");
-        }
+        JDBC.startConnection();
+        launch(args);
         JDBC.closeConnection();
     }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/Login.fxml")));
-        primaryStage.setTitle("First View");
-        primaryStage.setScene(new Scene(root, 800, 600));
-        primaryStage.show();
-    }
 
-}
+        @Override
+        public void start (Stage primaryStage) throws IOException {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/Login.fxml")));
+            primaryStage.setTitle("First View");
+            primaryStage.setScene(new Scene(root, 363, 260));
+            primaryStage.show();
+        }
+
+    }
