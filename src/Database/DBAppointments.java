c2ac4df2096537014e615b23276bf0c408011889
PorkChopExpress86 <29725019+PorkChopExpress86@users.noreply.github.com>
2022-07-23 09:51:05 -0500

    Used try/catch in getAllAppointments instead of throwing SQLException to match style for monthly and weekly table.

--- src/Database/DBAppointments.java
+++ src/Database/DBAppointments.java
@@ -1,7 +1,7 @@
 package Database;
 
-import Model.Appointment;
-import Model.Contact;
+import model.Appointment;
+import model.Contact;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -111,33 +111,40 @@ public class DBAppointments {
         return list;
     }
 
-    public static ObservableList<Appointment> getAllAppointments() throws SQLException {
+    public static ObservableList<Appointment> getAllAppointments() {
+
         ObservableList<Appointment> appointmentList = FXCollections.observableArrayList();
-        String sql = "SELECT * FROM appointments";
+        try {
+            String sql = "SELECT Appointment_ID, Title, Description, Location, Type, Start, End, Customer_ID, User_ID, Contact_ID  FROM appointments";
 
-        PreparedStatement ps = JDBC.getConnection().prepareStatement(sql);
-        ResultSet rs = ps.executeQuery();
+            PreparedStatement ps = JDBC.getConnection().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
 
-        while (rs.next()) {
-            int appointmentId = rs.getInt("Appointment_ID");
-            String title = rs.getString("Title");
-            String description = rs.getString("Description");
-            String location = rs.getString("Location");
-            String type = rs.getString("Type");
-            LocalDateTime startTime = rs.getTimestamp("Start").toLocalDateTime();
-            LocalDateTime endTime = rs.getTimestamp("End").toLocalDateTime();
-            int custId = rs.getInt("Customer_ID");
-            int userId = rs.getInt("User_ID");
-            int contactId = rs.getInt("Contact_ID");
-            Appointment appointment = new Appointment(appointmentId, title, description, location, type, startTime, endTime, custId, userId, contactId);
+            while (rs.next()) {
+                int appointmentId = rs.getInt("Appointment_ID");
+                String title = rs.getString("Title");
+                String description = rs.getString("Description");
+                String location = rs.getString("Location");
+                String type = rs.getString("Type");
+                LocalDateTime startTime = rs.getTimestamp("Start").toLocalDateTime();
+                LocalDateTime endTime = rs.getTimestamp("End").toLocalDateTime();
+                int custId = rs.getInt("Customer_ID");
+                int userId = rs.getInt("User_ID");
+                int contactId = rs.getInt("Contact_ID");
+                Appointment appointment = new Appointment(appointmentId, title, description, location, type, startTime, endTime, custId, userId, contactId);
 
-            //Add the appointment to the list
-            appointmentList.add(appointment);
+                //Add the appointment to the list
+                appointmentList.add(appointment);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return appointmentList;
     }
 
-    public static boolean updateAppointment(int appointmentId, String title, String description, String location, String type, LocalDateTime startDateAndTime, LocalDateTime endDateAndTime, int customerId, int userId, int contactId) throws SQLException {
+    public static boolean updateAppointment(int appointmentId, String title, String description, String
+            location, String type, LocalDateTime startDateAndTime, LocalDateTime endDateAndTime, int customerId, int userId,
+                                            int contactId) throws SQLException {
         Contact contact = DBContact.getContactById(contactId);
         String sql = "UPDATE appointments SET Title=?, Description=?, Location=? , Type=?, startDateAndTime=?, endDateAndTime=?, Customer_ID=?, User_ID=?, Contact_ID=? WHERE Appointment_ID=?";
 
